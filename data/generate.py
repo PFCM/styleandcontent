27643d105caa272de75ad52ea7b032eede05baa1
pfcm <pfcmathews@gmail.com>
2016-06-03 16:18:44 +1200

    add some data

---
+++ data/generate.py
@@ -0,0 +1,33 @@
+"""Script to generate the data from font files."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import sys
+import os
+import string
+
+import numpy as np
+
+from PIL import Image, ImageFont, ImageDraw
+
+
+def main():
+    filename = sys.argv[1]
+    print('Loading font from {}'.format(filename))
+    font = ImageFont.truetype(filename, 34)  # TODO size
+    output_folder = os.path.join(
+        os.path.dirname(__file__),
+        os.path.basename(filename).split('.')[0])
+    os.makedirs(output_folder, exist_ok=True)
+    print('~~saving to {}'.format(output_folder))
+    for char in string.ascii_letters:
+        image = Image.new('RGB', (32, 32))
+        draw = ImageDraw.Draw(image)
+        w, h = font.getsize(char)
+        draw.text(((32-w)/2, (28-h)/2), char, font=font)
+        image.save(os.path.join(output_folder, char)+'.png', 'PNG')
+    
+
+if __name__ == '__main__':
+    main()
